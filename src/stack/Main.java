7ce46bd689bb22af0efea447c306d8c33cd3bbbf
e-ricaud <emile.ricaud@gmail.com>
2025-08-05 23:30:08 +0200

    Add Valid Parenthese Algo Exercice

---
+++ src/stack/Main.java
@@ -0,0 +1,8 @@
+package stack;
+
+public class Main {
+  public static void main(String[] args) {
+    // Valid Parentheses
+    System.out.println("Is valid parentheses -> " + StackUtil.isValidParentheses("({[()]})"));
+  }
+}
--- src/stack/StackUtil.java
+++ src/stack/StackUtil.java
@@ -1,5 +1,9 @@
 package stack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+
 /**
  * Valid Parentheses
  * Min Stack
@@ -9,4 +13,31 @@ package stack;
  * Largest Rectangle in Histogram (plus avancé)
  */
 public class StackUtil {
+
+  public static boolean isValidParentheses(String s) {
+    if (s.isBlank()) {
+      throw new IllegalArgumentException("String cannot be empty");
+    }
+    Map<Character, Character> matchingParentheses = Map.of(
+            ')', '(',
+            ']', '[',
+            '}', '{'
+    );
+    Deque<Character> stack = new ArrayDeque<>();
+
+    for (int i = 0; i < s.length(); i++) {
+      Character c = s.charAt(i);
+      if (matchingParentheses.containsValue(c)) {
+        stack.addLast(c);
+      } else if (matchingParentheses.containsKey(c)) {
+        if (stack.isEmpty() || !stack.peekLast().equals(matchingParentheses.get(c))) {
+          return false;
+        }
+        stack.pollLast();
+      }
+    }
+    return stack.isEmpty();
+  }
+
+
 }
